64115486d0a23db036be1cd6152e2fcff0298bb9
Bub <darrenborromeodrb@gmail.com>
2023-07-13 13:55:32 +1000

    add enemy animation and logic

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
+signal died 
+
+@onready var hp_label = $Label
+@onready var animation_player = $AnimationPlayer
+
 var hp = 25: 
-	set = SetHp  # Called whenever it's set. 
+	set = SetHp
 
 func SetHp(new_hp): 
 	hp = new_hp
-	$Label.text = str(hp) + "hp"
+	hp_label.text = str(hp) + "hp"
+	
+	if hp <= 0: 
+		emit_signal("died") 
+		queue_free()
+	else: 		
+		animation_player.play("Shake") 
+		await animation_player.animation_finished
+		await get_tree().create_timer(0.25).timeout
+		animation_player.play("Attack")
--- Scripts/battle.gd
+++ Scripts/battle.gd
@@ -1,6 +1,12 @@
 extends Node
 
+@onready var enemy = $Enemy
+@onready var sword_button = $UI/SwordButton
+
 func _on_sword_button_pressed():
-	print("attack!") 
-	
-	$Enemy.hp -= 4
+	if enemy != null:
+		enemy.hp -= 4
+		
+func _on_enemy_died():
+	sword_button.disabled = true
+	enemy = null 
